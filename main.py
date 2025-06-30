afc91927aa6afbfea1a7284d2d6d58f27a3631c1
Raeone <pay124@seznam.cz>
2025-06-30 16:09:15 +0200

    Verbose: add --verbose flag to prompt

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from google.genai import types
 def main():
   load_dotenv()
 
-  args = sys.argv[1:]
+  is_verbose = "--verbose" in sys.argv
+  args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
   if not args:
     print("AI Code Assistant")
-    print('\nUsage: python main.py "your prompt here"')
+    print('\nUsage: python main.py "your prompt here" [--verbose]')
     print('Example: python main.py "How do I build a calculator app?"')
     sys.exit(1)
 
@@ -18,21 +19,28 @@ def main():
   client = genai.Client(api_key=api_key)
   
   user_prompt = " ".join(args)
-  
+  if is_verbose:
+    print(f"User prompt: {user_prompt}")
+
   messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)])
   ]
 
-  generate_content(client, messages)
+  generate_content(client, messages, is_verbose)
 
 
-def generate_content(client, messages):
+def generate_content(client, messages, is_verbose):
   response = client.models.generate_content(
       model='gemini-2.0-flash-001', contents=messages
   )
+
+  if is_verbose:
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
   print("Response:")
   print(response.text)
-  
+
 
 
 if __name__ == "__main__":
