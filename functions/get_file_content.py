c5c1beff54e9660f034d971e03de24e41f753b02
Raeone <pay124@seznam.cz>
2025-07-08 20:04:25 +0200

    bootdev solution

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -2,24 +2,19 @@ import os
 from config import MAX_CHARS
 
 def get_file_content(working_directory, file_path):
-  file_path_rel = os.path.join(working_directory, file_path)
-  file_path_abs = os.path.abspath(file_path_rel)
-
-  if not file_path_abs.startswith(os.path.abspath(working_directory)):
+  abs_working_dir = os.path.abspath(working_directory)
+  abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+  if not abs_file_path.startswith(abs_working_dir):
     return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-  
-  if not os.path.isfile(file_path_abs):
+  if not os.path.isfile(abs_file_path):
     return f'Error: File not found or is not a regular file: "{file_path}"'
-  
   try:
-    with open(file_path_abs) as file:
-      file_content = file.read(MAX_CHARS)
-      if len(file_content) >= 10000:
-        return file_content + f'[...File "{file_path}" truncated at 10000 characters]'
-      return file_content
-
+    with open(abs_file_path, "r") as f:
+      content = f.read(MAX_CHARS)
+      if os.path.getsize(abs_file_path) > MAX_CHARS:
+        content += (
+          f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        )
+    return content
   except Exception as e:
-    return f'Error: {e}'
-  
-
-
+    return f'Error reading file "{file_path}": {e}'
