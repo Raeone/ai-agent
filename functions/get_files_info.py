d47a2706dcb7728493a08ec992863d4d11f3efce
Raeone <pay124@seznam.cz>
2025-07-08 12:57:45 +0200

    add comment

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,6 +1,16 @@
 import os
 
 def get_files_info(working_directory, directory=None):
+  """Function for AI. It will return all directories and files in directory (which must be inside working_directory), its names, file size and if it is file or directory.
+  It's best practice to limit access for AI - here it's working_directory and if not target_dir.startswith() condition. 
+
+  Args:
+      working_directory (string): Directory to which AI has access - this limitation is a must hava, so AI doesn't have access to all or confidential data
+      directory (string, optional): Directory to look. Defaults to None.
+
+  Returns:
+      string: file_name, file size and boole if directory (or file)
+  """
   abs_working_dir = os.path.abspath(working_directory)
   target_dir = abs_working_dir
     
