449cae8ddb95f7cea31d6bbe52f6ddc1b0ba6a64
Raeone <pay124@seznam.cz>
2025-07-09 15:30:07 +0200

    write_file function

--- tests.py
+++ tests.py
@@ -1,28 +1,10 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def test():
-    print(get_file_content("calculator", "lorem.txt"))
-    print(get_file_content("calculator", "main.py"))
-    print(get_file_content("calculator", "pkg/calculator.py"))
-    print(get_file_content("calculator", "/bin/cat"))
+    print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
+    print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+    print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+    
     
 if __name__ == "__main__":
     test()
-
-def get_file_content(working_directory, file_path):
-    abs_working_dir = os.path.abspath(working_directory)
-    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
-    if not abs_file_path.startswith(abs_working_dir):
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(abs_file_path):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
-    try:
-        with open(abs_file_path, "r") as f:
-            content = f.read(MAX_CHARS)
-            if os.path.getsize(abs_file_path) > MAX_CHARS:
-                content += (
-                    f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
-                )
-        return content
-    except Exception as e:
-        return f'Error reading file "{file_path}": {e}'
