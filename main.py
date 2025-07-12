98a6c92483a2a6decb898772d910b4d7a1170e48
Raeone <pay124@seznam.cz>
2025-07-12 18:01:12 +0200

    call_function

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+from prompts import system_prompt
+from call_function import available_functions
+
 def main():
   load_dotenv()
 
@@ -31,17 +34,23 @@ def main():
 
 def generate_content(client, messages, is_verbose):
   response = client.models.generate_content(
-      model='gemini-2.0-flash-001', contents=messages
+      model='gemini-2.0-flash-001', 
+      contents=messages,
+      config=types.GenerateContentConfig(
+        tools=[available_functions],
+        system_instruction=system_prompt
+      ),      
   )
 
   if is_verbose:
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    
-  print("Response:")
-  print(response.text)
+  
+  if not response.function_calls:
+    return response.text
 
-
+  for function_call_part in response.function_calls:
+    print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 if __name__ == "__main__":
   main()
--- tests.py
+++ tests.py
@@ -1,15 +1,7 @@
-from functions.run_python_file import run_python_file
+from functions.get_files_info import get_files_info
 
 def test():
-    print("1.")
-    print(run_python_file("calculator", "main.py"))
-    print("2.")
-    print(run_python_file("calculator", "tests.py"))    
-    print("3.")
-    print(run_python_file("calculator", "../main.py"))
-    print("4.")
-    print(run_python_file("calculator", "nonexistent.py"))
+    
     
 if __name__ == "__main__":
     test()
-
